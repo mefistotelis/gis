06350f56915425f1057d407dac997abaa1641e6e
Mefistotelis <mefistotelis@gmail.com>
2020-11-20 16:52:09 +0100

    Commented the old dl script

--- download_doplaty-2016.sh
+++ download_doplaty-2016.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+# This script downloads a whole ortophoto map from Polish agricultular maps
+# The maps are only available for registered farmers, who have login to the
+# direct subsides application system.
+# Usage:
+# Log in to the service, view the map within it, then use "Export cookies"
+# plugin to create cookies-*.txt. Rename the cookie file and place with
+# this script. The whole map of Poland will get downloaded.
+# Make sure you have a lot of free disk space. You can also narrow down
+# the x and y range to download only a chunk of the whole map.
+
 #set -x
 
-# Log in to the service, view the map within it, then use "Export cookies" plugin to create cookies.txt.
-sessionid=$(sed -n 's/^interop[.]doplaty[.]gov[.]pl	.*	\/lids\/browser	.*	JSESSIONID	\(.*\)$/\1/p' cookies.txt | tr -d '\r\n')
+sessionid=$(sed -n 's/^interop[.]doplaty[.]gov[.]pl	.*	\/lids\/browser	.*	JSESSIONID	\(.*\)$/\1/p' cookies-2016.txt | tr -d '\r\n')
 
 for z in {7..7}; do
   for x in {8..1219}; do # whole width of poland
@@ -19,7 +28,7 @@ for z in {7..7}; do
       fi
       curl \
         -A "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3" \
-        --cookie cookies.txt "https://interop.doplaty.gov.pl/lids/browser/proxy/TileMapService/tms_ortofotomapa/${z}/${x}/${y}.png;jsessionid=${sessionid}?" \
+        --cookie cookies-2016.txt "https://interop.doplaty.gov.pl/lids/browser/proxy/TileMapService/tms_ortofotomapa/${z}/${x}/${y}.png;jsessionid=${sessionid}?" \
         > "${fname}"
         fsize=$(wc -c <"${fname}")
       if [ "${fsize}" -lt "256" ]; then
